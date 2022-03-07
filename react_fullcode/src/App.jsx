69a1d705607f1b0b69ea74c2fe2fdadfa520ab1f
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2022-03-07 15:29:37 +0500

    Add useState,component

--- react_fullcode/src/App.jsx
+++ react_fullcode/src/App.jsx
@@ -1,46 +1,32 @@
-
 import { useState } from 'react';
 import './App.css';
+import Btn from './Component/Btn';
 
 function App() {
-  const [count,setCount] =  useState(0);
   const [toggleBtn,setToggleBtn] =  useState(false);
-  function inc(){
-    setCount(prev => prev+1)
-  }
+  const [value,setValue] =  useState("Salomlar");
 
-  function dec(){
-    setCount(prev => prev-1)
-  }
-
-  return (<>
-      <div className="app w-50 mx-auto">
-        <h3>
-          Count: {count}</h3>
-          
-            <button className="
-            btn btn-success" 
-            onClick={inc}>
-              Incr
-            </button>
-            {/* <h3>
-          Count: {count}</h3>
-          
-            <button className="
-            btn btn-success" 
-            onClick={dec}>
-              Incr
-            </button> */}
-
-            <button 
-            onClick={() => setToggleBtn(!toggleBtn)}
+return (<>
+  <div className="app w-50 mx-auto">
+    <Btn />
+    <hr />
+    <p>Value: {value}</p>
+    <button 
+            onClick={() => 
+            setToggleBtn(!toggleBtn)}
             className="btn btn-secondary">
               Toggle Btn
               </button>
-              {toggleBtn ? <p className='lead'>Abdullajon shakarsan, shakar</p> : null}
-          </div>
-     </>
-  );
+    <input type="text" className='form-control' value={value} onChange={event=> setValue(event.target.value)} />
+    {toggleBtn ?
+    <p className='lead'>
+      Abdullajon shakarsan,
+      shakar</p>
+    :
+    null}
+  </div>
+</>
+);
 }
 
 export default App;
---
+++ react_fullcode/src/Component/Btn.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Btn  () {
+  const [count,setCount] =  useState(0);
+ 
+  
+  function inc(){
+    setCount(prev => prev+1)
+  }
+
+  function dec(){
+    setCount(prev => prev-1)
+  }
+
+  return (
+    <>
+          <h3>
+          Count: {count}</h3>
+          
+            <button className="
+            btn btn-success" 
+            onClick={inc}>
+              Incr
+            </button>
+            
+          
+            <button className="
+            btn btn-danger" 
+            onClick={dec}>
+              Incr
+            </button> 
+
+         
+             
+    </>
+  )
+}
