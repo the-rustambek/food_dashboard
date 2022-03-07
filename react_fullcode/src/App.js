89f223cf579999182891195bdb1b41bea03fba34
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2022-03-07 14:21:46 +0500

    Add beginner npm

---
+++ react_fullcode/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (<>
+      <div variant="danger" className='card bg-danger bg-primary '> 
+          nima gap lorem15
+      </div>
+     </>
+  );
+}
+
+export default App;
